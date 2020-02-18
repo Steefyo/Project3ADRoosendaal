afb7d1208ff4c89c87b02942a537c50d31b6657f
StickFunFight <36295961+StickFunFight@users.noreply.github.com>
2020-02-18 13:05:45 +0100

    Videopagina aangemaakt en gevuld met video (#15)
    * Update styles.xml
    
    Standaard font veranderd naar roman
    
    * Mappen structuur en top bar weg
    
    heb een nieuwe package toegevoegd genaamd controller en model ook heb ik de top bar verwijderd
    
    * video pagina aangemaakt en gevuld
    
    video pagina aangemaakt en gevuld

--- app/src/main/java/com/example/onboarding/Controller/Main/MainActivity.java
+++ app/src/main/java/com/example/onboarding/Controller/Main/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.onboarding.Controller.Main;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.example.onboarding.Controller.VideoPagina.VideoPagina;
 import com.example.onboarding.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,4 +16,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void Send(View view) {
+        Intent intent = new Intent(this, VideoPagina.class);
+        startActivity(intent);
+    }
+
 }
