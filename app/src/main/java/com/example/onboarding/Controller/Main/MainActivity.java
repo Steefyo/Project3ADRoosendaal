7990dd65e14411cca4c6e9a5801db1fa69262ffd
larsupars <31402411+larsupars@users.noreply.github.com>
2020-02-18 14:16:42 +0100

    Master (#17)
    * Videopagina (#12)
    
    * Update styles.xml
    
    Standaard font veranderd naar roman
    
    * Mappen structuur en top bar weg
    
    heb een nieuwe package toegevoegd genaamd controller en model ook heb ik de top bar verwijderd
    
    Co-authored-by: StickFunFight <36295961+StickFunFight@users.noreply.github.com>
    
    * Create .gitignore (#13)
    
    * Develop (#14)
    
    * Update styles.xml (#11)
    
    Standaard font veranderd naar roman
    
    * Welkomstscherm gecreëerd
    
    Co-authored-by: StickFunFight <36295961+StickFunFight@users.noreply.github.com>
    
    * Videopagina aangemaakt en gevuld met video (#15)
    
    * Update styles.xml
    
    Standaard font veranderd naar roman
    
    * Mappen structuur en top bar weg
    
    heb een nieuwe package toegevoegd genaamd controller en model ook heb ik de top bar verwijderd
    
    * video pagina aangemaakt en gevuld
    
    video pagina aangemaakt en gevuld
    
    Co-authored-by: Steefyo <33659399+Steefyo@users.noreply.github.com>
    Co-authored-by: StickFunFight <36295961+StickFunFight@users.noreply.github.com>

---
+++ app/src/main/java/com/example/onboarding/Controller/Main/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.onboarding.Controller.Main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.onboarding.Controller.VideoPagina.VideoPagina;
+import com.example.onboarding.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Send(View view) {
+        Intent intent = new Intent(this, VideoPagina.class);
+        startActivity(intent);
+    }
+
+}
