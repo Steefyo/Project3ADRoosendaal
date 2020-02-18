f4e593f24662791df087c94f89eb2b777129f3f7
StickFunFight <36295961+StickFunFight@users.noreply.github.com>
2020-02-18 13:01:31 +0100

    video pagina aangemaakt en gevuld
    video pagina aangemaakt en gevuld

--- app/src/main/java/com/example/onboarding/Controller/Main/MainActivity.java
+++ app/src/main/java/com/example/onboarding/Controller/Main/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.onboarding.Controller.Main;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.example.onboarding.Controller.VideoPagina.VideoPagina;
 import com.example.onboarding.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +15,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void Send(View view) {
+        Intent intent = new Intent(this, VideoPagina.class);
+        startActivity(intent);
     }
 }
