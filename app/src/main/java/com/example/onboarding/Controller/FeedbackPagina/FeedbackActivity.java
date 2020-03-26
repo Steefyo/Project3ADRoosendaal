379a31675f60c2e4294c00956626324539adb216
RemcoMoerland <60404331+RemcoMoerland@users.noreply.github.com>
2020-03-26 10:45:54 +0100

    Info en Feedback zijn afgerond.
    De Info pagina heeft een visuele update gehad en de Feedback pagina heeft naast een visuele update ook een API/database connectie gekregen.

--- app/src/main/java/com/example/onboarding/Controller/FeedbackPagina/FeedbackActivity.java
+++ app/src/main/java/com/example/onboarding/Controller/FeedbackPagina/FeedbackActivity.java
@@ -6,30 +6,90 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.example.onboarding.Controller.MenuPagina.MenuActivity;
+import com.example.onboarding.Helpers.VolleyHelper;
 import com.example.onboarding.R;
 
-public class FeedbackActivity extends AppCompatActivity {
+import org.json.JSONObject;
 
-    private Button buttonTerug;
+public class FeedbackActivity extends AppCompatActivity implements Response.Listener<JSONObject>, Response.ErrorListener {
+
+    String studentId;
+    String opmerking;
+    String rating;
+
+    private RadioGroup radioGroup;
+    private RadioButton radioButton;
+    private Button buttonAfronden;
+    private VolleyHelper helper;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
 
-        buttonTerug = findViewById(R.id.buttonTerug);
-        buttonTerug.setOnClickListener(new View.OnClickListener() {
+        if(savedInstanceState == null) {
+            Bundle extras = getIntent().getExtras();
+            if(extras == null) {
+                studentId = null;
+            } else {
+                studentId = extras.getString("StudentId");
+            }
+        } else {
+            studentId = (String) savedInstanceState.getSerializable("StudentId");
+        }
+
+        buttonAfronden = findViewById(R.id.buttonAfronden);
+        buttonAfronden.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 sluitFeedbackActivity();
+
             }
         });
     }
 
+
     public void sluitFeedbackActivity() {
+
+        radioGroup = findViewById(R.id.radio);
+
+        int selectedId = radioGroup.getCheckedRadioButtonId();
+
+        View rb = radioGroup.findViewById(selectedId);
+        radioButton = (RadioButton) radioGroup.getChildAt(radioGroup.indexOfChild(rb));
+
+        rating = radioButton.getText().toString();
+
+        EditText editText = (EditText) findViewById(R.id.editText2);
+
+        opmerking = editText.getText().toString();
+
+        helper = new VolleyHelper(getBaseContext(), "https://adaonboarding.ml/t2/");
+                helper.get("InsertFeedback.php?studentId="+studentId+"&opmerking="+opmerking+"&rating="+rating, null, this, this);
+
+
         Intent intent = new Intent(this, MenuActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        System.out.println(error);
+    }
+
+    @Override
+    public void onResponse(JSONObject response) {
+
+        System.out.println("error");
+
     }
 }
--- app/src/main/java/com/example/onboarding/Helpers/VolleyHelper.java
+++ app/src/main/java/com/example/onboarding/Helpers/VolleyHelper.java
@@ -8,6 +8,7 @@ import com.android.volley.Response;
 import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.example.onboarding.Controller.FeedbackPagina.FeedbackActivity;
 
 import org.json.JSONObject;
 
@@ -59,5 +60,4 @@ public class VolleyHelper {
         JsonObjectRequest objRequest = new JsonObjectRequest(Request.Method.DELETE, contructUrl(method), jsonRequest, listener, errorListener);
         requestQueue.add(objRequest);
     }
-
 }
