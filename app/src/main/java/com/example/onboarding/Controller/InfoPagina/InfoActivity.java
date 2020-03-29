486d83dd123064b994068a31def7864c5484c236
RemcoMoerland <60404331+RemcoMoerland@users.noreply.github.com>
2020-03-29 18:26:06 +0200

    Info feedback paginas (#34)
    * Info en Feedback zijn afgerond.
    
    De Info pagina heeft een visuele update gehad en de Feedback pagina heeft naast een visuele update ook een API/database connectie gekregen.
    
    * Referentie weggehaald en comments toegevoegd
    
    * Kleine aanpassingen en UI update
    
    Ik heb de vierkante, blauwe doos rond de welkom tekst gezet, wat hardcoded tekst dynamisch gemaakt. De fasevoortgang code geimplementeerd in mijn info pagina, en wat fields dynamisch gemaakt in grootte.
    
    * Update activity_feedback.xml
    
    Co-authored-by: StickFunFight <36295961+StickFunFight@users.noreply.github.com>

--- app/src/main/java/com/example/onboarding/Controller/InfoPagina/InfoActivity.java
+++ app/src/main/java/com/example/onboarding/Controller/InfoPagina/InfoActivity.java
@@ -2,16 +2,23 @@ package com.example.onboarding.Controller.InfoPagina;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.example.onboarding.Controller.MenuPagina.MenuActivity;
+import com.example.onboarding.Helpers.VolleyHelper;
 import com.example.onboarding.R;
 
-public class InfoActivity extends AppCompatActivity {
+import org.json.JSONObject;
 
+public class InfoActivity extends AppCompatActivity implements Response.Listener<JSONObject>, Response.ErrorListener{
+
+    private VolleyHelper helper;
     private Button buttonTerug;
 
     String studentId;
@@ -44,8 +51,21 @@ public class InfoActivity extends AppCompatActivity {
 
     //Functie om info weer af te sluiten en terug naar het menu te gaan.
     public void sluitInfoActivity() {
+        helper = new VolleyHelper(getBaseContext(), "https://adaonboarding.ml/t2/");
+        helper.get("InsertFase.php?FaseNaam=faseAboutR&StudentId="+studentId, null, this, this);
         Intent intent = new Intent(this, MenuActivity.class);
         intent.putExtra("StudentId", studentId);
         startActivity(intent);
+
     }
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        Log.e("Volley Error", error.toString());
+    }
+
+    @Override
+    public void onResponse(JSONObject response) {
+
+    }
+
 }
